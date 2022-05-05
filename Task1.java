07b448d26259d12419fe5a67c572bd0f35d3d938
Knyaz606 <knyaz349@mail.ru>
2022-05-05 11:02:54 +0400

    Update Task1.java

--- Task1.java
+++ Task1.java
@@ -3,11 +3,11 @@ public class Task1 {
         public static void main(String[] args) {
 
             Scanner num = new Scanner(System.in);
-            int chislo, bad = 7;
+            int chislo = 7;
             System.out.println("Угадай число:\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10");
             chislo = num.nextInt();
 
-            if (chislo == bad) {
+            if (chislo == 7) {
                 System.out.print("Вы угадали");
             }else if (chislo >=7) {
                 System.out.print("Ваше число больше");
