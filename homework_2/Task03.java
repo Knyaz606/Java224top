94ac9bd6133c13ca207ebf1ea03633d9455d3d67
Anatoli Knyaz <knyaz349@mail.ru>
2022-05-27 21:49:33 +0400

    Create Task03.java

---
+++ homework_2/Task03.java
@@ -0,0 +1,8 @@
+package homework_2;
+
+public class Task03 {
+    public static void main(String[] args) {
+        float n = 4.6f;
+        System.out.println("Округление числа = " + Math.round(n));
+    }
+}
