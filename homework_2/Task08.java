3c445879f130fb41a247ad9808fe6488926fa9e1
Anatoli Knyaz <knyaz349@mail.ru>
2022-05-31 20:39:54 +0400

    Create Task08.java

---
+++ homework_2/Task08.java
@@ -0,0 +1,10 @@
+package homework_2;
+
+public class Task08 {
+    public static void main(String[] args) {
+        int s = 340; //скорость звука
+        final int w = 5; //время между молнией и звуком
+        int r = s * w; //растояние удара молнии
+        System.out.println("Расстояние удара молнии " + r + " метров"); // вывод
+    }
+}
