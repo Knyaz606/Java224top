86fca16165151404797b32e5b6c7a26647768fc8
Anatoli Knyaz <knyaz349@mail.ru>
2022-05-31 20:39:56 +0400

    Create Task10.java

---
+++ homework_2/Task10.java
@@ -0,0 +1,11 @@
+package homework_2;
+
+public class Task10 {
+    public static void main(String[] args) {
+
+        final int height = 190;
+        final int weight = 62;
+        int itog = (190 - 110) - weight;
+        System.out.println("нужно набрать " + itog);
+    }
+}
