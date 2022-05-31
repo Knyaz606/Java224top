badb7667386c3791dc096ef337ed3dcc8eb2ff3d
Anatoli Knyaz <knyaz349@mail.ru>
2022-05-31 20:39:51 +0400

    Create Task09.java

---
+++ homework_2/Task09.java
@@ -0,0 +1,15 @@
+package homework_2;
+
+public class Task09 {
+    public static void main(String[] args) {
+        double b = 10.0;
+        if (b % 1 ==0){
+            System.out.println(b);
+            double a = 13.14;
+            if (a % 1 ==0){
+                System.out.println(a);
+            }
+
+        }
+    }
+}
