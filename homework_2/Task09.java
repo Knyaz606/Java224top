92986906afa7c2d804a32bfb5b84da4e33a459b1
Anatoli Knyaz <knyaz349@mail.ru>
2022-06-01 21:44:43 +0400

    Update Task09.java

--- homework_2/Task09.java
+++ homework_2/Task09.java
@@ -3,13 +3,11 @@ package homework_2;
 public class Task09 {
     public static void main(String[] args) {
         double b = 10.0;
-        if (b % 1 ==0){
-            System.out.println(b);
+            System.out.println(Math.round(b));
             double a = 13.14;
-            if (a % 1 ==0){
-                System.out.println(a);
+                System.out.println(Math.round(a));
             }
 
         }
-    }
-}
+
+
